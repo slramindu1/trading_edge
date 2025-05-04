2595db0a6733562569feae414fc8b5ee05f3c2c0
slramindu1 <rmaindu.jia@gmail.com>
2025-05-04 10:12:46 +0530

    updated title

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,21 +3,21 @@ import { Metadata } from "next";
 export const SITE_CONFIG: Metadata = {
     title: {
         // write a default title for astra a ai powered website builder suggest something unique and catchy don't use the same words of ai powered website builder give a unique name
-        default: "Astra - AI Powered Website Builder",
-        template: `%s | Astra`
+        default: "Trading Edge ",
+        template: `%s | `
     },
-    description: "Astra is an AI powered website builder that helps you create a website in minutes. No coding skills required. Get started for free!",
+    description: "Learn Forx Trading On The Lowest Price In Sri Lnaka",
     icons: {
         icon: [
-            {
-                url: "/icons/favicon.ico",
-                href: "/icons/favicon.ico",
-            }
+            // {
+            //     url: "/icons/favicon.ico",
+            //     href: "/icons/favicon.ico",
+            // }
         ]
     },
     openGraph: {
-        title: "Astra - AI Powered Website Builder",
-        description: "Astra is an AI powered website builder that helps you create a website in minutes. No coding skills required. Get started for free!",
+        title: "Trading Edge ",
+        description: "Learn Forx Trading On The Lowest Price In Sri Lnaka",
         images: [
             {
                 url: "/assets/og-image.png",
@@ -27,13 +27,13 @@ export const SITE_CONFIG: Metadata = {
     twitter: {
         card: "summary_large_image",
         creator: "@shreyassihasane",
-        title: "Astra - AI Powered Website Builder",
-        description: "Astra is an AI powered website builder that helps you create a website in minutes. No coding skills required. Get started for free!",
+        title: "Trading Edge ",
+        description: "Learn Forx Trading On The Lowest Price In Sri Lnaka",
         images: [
             {
                 url: "/assets/og-image.png",
             }
         ]
     },
-    metadataBase: new URL("https://astra-app.vercel.app"),
+    metadataBase: new URL("#"),
 };
