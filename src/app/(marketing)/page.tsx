1627f31ca980f9904615d9e5fc47b572eead0762
slramindu1 <Ramindu.jiat@gmail.com>
2025-12-24 18:40:21 +0530

    Update Testimonails

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -573,21 +573,25 @@ const HomePage = () => {
             <div className="columns-1 md:columns-2 lg:columns-3 gap-4 space-y-4">
               {[
                 "/assets/5.jpg",
-                "/assets/2.jpg",
-                "/assets/3.jpg",
+                "/assets/new3.jpg",
+                "/assets/new7.jpg",
                 "/assets/4.jpg",
-                "/assets/1.jpg",
-                "/assets/6.jpg",
-                "/assets/7.jpg",
-                "/assets/8.jpg",
-                "/assets/9.jpg",
-                "/assets/10.jpg",
+                "/assets/new10.jpg",
+                "/assets/new13.jpg",
+                "/assets/new1.jpg",
+                "/assets/new4.jpg",
+                "/assets/new5.jpg",
+                "/assets/new8.jpg",
+                "/assets/new11.jpg",
                 "/assets/11.jpg",
-                "/assets/12.jpg",
-                "/assets/13.jpg",
-                "/assets/14.jpg",
-                "/assets/15.jpg",
-                "/assets/16.jpg",
+                "/assets/new2.jpg",
+                "/assets/new6.jpg",
+                "/assets/new9.jpg",
+                "/assets/new12.jpg",
+                "/assets/new14.jpg",
+                
+            
+              
               ].map((src, index) => (
                 <div
                   key={index}
